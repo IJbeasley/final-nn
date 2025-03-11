4bdcababf952fa08131cc0f5cba025106d46948b
Isobel Beasley <isobel.beasley@ucsf.edu>
2025-03-10 20:28:13 -0700

    Adding forward function

--- nn/nn.py
+++ nn/nn.py
@@ -135,9 +135,41 @@ class NeuralNetwork:
             cache: Dict[str, ArrayLike]:
                 Dictionary storing Z and A matrices from `_single_forward` for use in backprop.
         """
+        # Initialise variables
+        cache = {}
+        
+        # Add first layer to cache (this is just the input layer)
+        cache['A0'] = X
+        
+        # First A_prev is the input
+        A_prev = X
+        
+        # Traverse each layer .. 
+        for idx, layer in enumerate(self.arch):
+            layer_idx = idx + 1
+            
+            # Get parameters required for _single_forward
+            activation = layer['activation']
+            W_curr = self._param_dict['W' + str(layer_idx)]
+            b_curr = self._param_dict['b' + str(layer_idx)]
+            
+            A_curr, Z_curr = self._single_forward(
+                                                  W_curr,
+                                                  b_curr,
+                                                  A_prev,
+                                                  activation
+                                                  ) 
+           
+            # Update cache
+            cache['A' + str(layer_idx)] = A_curr
+            cache['Z' + str(layer_idx)] = Z_curr
+            
+            # Update A_prev for next layer
+            A_prev = A_curr
+        
+        output = A_prev
         
         return output, cache
-        pass
 
     def _single_backprop(
         self,
