e56a203f8dbf150ecfc92fe54cb30a2dbdb789c4
Isobel Beasley <ibeasley@student.unimelb.edu.au>
2025-03-13 14:22:00 -0700

    Fixing unit test for mean squared error

--- test/test_nn.py
+++ test/test_nn.py
@@ -29,10 +29,21 @@ def test_mean_squared_error():
     """
     Ensure that the mean_squared_error function correctly calculates the mean squared error
     """
+
+    nn_eg_model  = NeuralNetwork(nn_arch = [{'input_dim': 64, 'output_dim': 16, 'activation': 'relu'},
+                                                                          {'input_dim': 16, 'output_dim': 64, 'activation': 'relu'}],
+                            lr = 0.5, 
+                            seed = 42, 
+                            batch_size = 5, 
+                            epochs = 1, 
+                            loss_function='mse', 
+                            verbose=False
+                                                    )
+
     y_true = np.array([1, 0, 1, 0])
     y_pred = np.array([0.9, 0.1, 0.8, 0.2])
 
-    mse = mean_squared_error(y_true, y_pred)
+    mse = nn_eg_model.mean_squared_error(y_true, y_pred)
 
     assert np.isclose(mse, 0.035), "Mean squared error is incorrect"
 
@@ -40,13 +51,23 @@ def test_mean_squared_error_backprop():
     pass
 
 def test_sample_seqs():
+    """
+    Ensure that the sample_seqs function correctly samples sequences
+    """
+
+    seqs = ['AGA', 'TGC', 'CTA', 'GAT']
+    labels = [True, False, True, False]
+
+
     pass
 
 def test_one_hot_encode_seqs():
     """
-    Ensure that the one_hot_encode_seqs function correctly encodes sequences
+    Ensure that the one_hot_encode_seqs function correctly one-hot-encodes sequences. 
+    Checks that one-hot-encoded sequence is correct length, and correct encoding.
     """
     encoded_seqs = one_hot_encode_seqs('AGA') 
 
+    assert len(encoded_seqs[0]) == 3 * 4, "One-hot encoding is incorrect"
 
     assert np.allequal(encoded_seqs, [1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0]), "One-hot encoding is incorrect"
